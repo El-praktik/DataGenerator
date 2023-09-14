6fa2d03e5db9a90d6b2f8b51d9e9aabab5079b1f
Alex Griebel Jensen <jindoohex@hotmail.com>
2023-09-14 17:44:13 +0200

    Add project files.

---
+++ Model/Meter.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace DataGenerator.Model
+{
+    public class Meter
+    {
+        private static int id = 1;
+
+        public int MID { get; set; }
+        public double PowerUsed { get; set; }
+        public double PowerGenerated { get; set; }
+        public int CID { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public Meter()
+        {
+            CID = 1;
+            MID = id++;
+        }
+
+        public Meter(int meterId, double powerUsed, double powerGenerated, int communityId, DateTime timeStamp)
+        {
+            MID = meterId;
+            PowerUsed = powerUsed;
+            PowerGenerated = powerGenerated;
+            CID = communityId;
+            Timestamp = timeStamp;
+        }
+
+        public void AddHourToTimestamp()
+        {
+            Timestamp = Timestamp.AddHours(1);
+
+        }
+
+        public void AddDayToTimestamp()
+        {
+            Timestamp = Timestamp.AddDays(1);
+
+        }
+
+        public void SetPowerUsed(double powerUsed)
+        {
+            Random random = new Random();
+            PowerUsed = powerUsed + random.Next(10, 20) * 10.87;
+        }
+
+        public void SetPowerGenerated(double powerGenerated)
+        {
+            Random random = new Random();
+            PowerGenerated = powerGenerated + random.Next(1, 7) * 51.87;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using DataGenerator.Model;
+using System.Data;
+using System.Data.SqlClient;
+
+// See https://aka.ms/new-console-template for more information
+
+string connectionString = "CONNECTION STRING";
+// string connectionString = "";
+// string connectionString = "";
+
+using SqlConnection connection = new SqlConnection(connectionString);
+connection.Open();
+
+string insertQuery = "INSERT INTO Measures (MID, PowerUsed, PowerGenerated, CID, TimestampFrom, TimestampTo) VALUES (@MeterId, @PowerUsed, @PowerGenerated, @CommunityId, @TimestampFrom, @TimestampTo)";
+
+Random random = new Random();
+
+// Create a list of ten meters
+List<Meter> meters = new List<Meter>();
+for (int i = 1; i <= 10; i++)
+{
+    meters.Add(new Meter { MID = i });
+}
+
+// Iterate through each meter
+foreach (Meter meter in meters)
+{
+    for (int hour = 1; hour <= 24; hour++)
+    {
+        // Generate data for each hour
+        meter.AddHourToTimestamp();
+        meter.SetPowerUsed(20);
+        meter.SetPowerGenerated(20);
+
+        // Create and execute the SQL command for inserting data
+        using SqlCommand command = new SqlCommand(insertQuery, connection);
+        command.Parameters.Add("@MeterId", SqlDbType.Int).Value = meter.MID;
+        command.Parameters.Add("@PowerUsed", SqlDbType.Float).Value = meter.PowerUsed;
+        command.Parameters.Add("@PowerGenerated", SqlDbType.Float).Value = meter.PowerGenerated;
+        command.Parameters.Add("@CommunityId", SqlDbType.Int).Value = meter.CID;
+        command.Parameters.Add("@Timestamp", SqlDbType.DateTime).Value = meter.Timestamp;
+
+        int rowsAffected = command.ExecuteNonQuery();
+        Console.WriteLine($"{rowsAffected} row(s) inserted for MeterId: {meter.MID}, Hour: {hour}");
+    }
+}
+
+connection.Close();
